62ac7e11375d48ca065e5885ff1983a0034b0372
doge <110700029+tototi932@users.noreply.github.com>
2022-09-13 00:16:24 -0400

    Update meta.ts

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -12,47 +12,47 @@ export const getCustomMeta = (path: string, t: ContextApi['t']): PageMeta => {
   switch (path) {
     case '/':
       return {
-        title: `${t('Home')} | ${t('BridgeSwap')}`,
+        title: `${t('Home')} | ${t('DogeGoldFlokiSwap')}`,
       }
     case '/competition':
       return {
-        title: `${t('Trading Battle')} | ${t('BridgeSwap')}`,
+        title: `${t('Trading Battle')} | ${t('DogeGoldFlokiSwap')}`,
       }
     case '/prediction':
       return {
-        title: `${t('Prediction')} | ${t('BridgeSwap')}`,
+        title: `${t('Prediction')} | ${t('DogeGoldFlokiSwap')}`,
       }
     case '/farms':
       return {
-        title: `${t('Farms')} | ${t('BridgeSwap')}`,
+        title: `${t('Farms')} | ${t('DogeGoldFlokiSwap')}`,
       }
     case '/pools':
       return {
-        title: `${t('Pools')} | ${t('BridgeSwap')}`,
+        title: `${t('Pools')} | ${t('DogeGoldFlokiSwap')}`,
       }
     case '/lottery':
       return {
-        title: `${t('Lottery')} | ${t('BridgeSwap')}`,
+        title: `${t('Lottery')} | ${t('DogeGoldFlokiSwap')}`,
       }
     case '/collectibles':
       return {
-        title: `${t('Collectibles')} | ${t('BridgeSwap')}`,
+        title: `${t('Collectibles')} | ${t('DogeGoldFlokiSwap')}`,
       }
     case '/ifo':
       return {
-        title: `${t('Initial Farm Offering')} | ${t('BridgeSwap')}`,
+        title: `${t('Initial Farm Offering')} | ${t('DogeGoldFlokiSwap')}`,
       }
     case '/teams':
       return {
-        title: `${t('Leaderboard')} | ${t('BridgeSwap')}`,
+        title: `${t('Leaderboard')} | ${t('DogeGoldFlokiSwap')}`,
       }
     case '/profile/tasks':
       return {
-        title: `${t('Task Center')} | ${t('BridgeSwap')}`,
+        title: `${t('Task Center')} | ${t('DogeGoldFlokiSwap')}`,
       }
     case '/profile':
       return {
-        title: `${t('Your Profile')} | ${t('BridgeSwap')}`,
+        title: `${t('Your Profile')} | ${t('DogeGoldFlokiSwap')}`,
       }
     default:
       return null
