d62b8e3778e876ab59897756750e7b7e0a2493f1
doge <110700029+tototi932@users.noreply.github.com>
2022-09-14 18:38:24 -0300

    Update index.esm.js

--- vital-folders/@pancakeswap/uikit/dist/index.esm.js
+++ vital-folders/@pancakeswap/uikit/dist/index.esm.js
@@ -3381,7 +3381,7 @@ var Icon$c = function (props) {
 
 var Icon$b = function (props) {
     return (React.createElement(Svg, __assign({ viewBox: "0 0 30 30", fill: "none" }, props),
-        React.createElement("circle", { cx: "15", cy: "15", r: "15", fill: "#09092B" }),
+        React.createElement("circle", { cx: "15", cy: "15", r: "15", fill: "#ffd348" }),
         React.createElement("path", { d: "M8.13778 9.90912H11.5511L15.1562 18.7046H15.3097L18.9148 9.90912H22.3281V23H19.6435V14.4794H19.5348L16.147 22.9361H14.3189L10.9311 14.4475H10.8224V23H8.13778V9.90912Z", fill: "#000000" })));
 };
 
