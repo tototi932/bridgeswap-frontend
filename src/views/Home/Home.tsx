137880a60a110574334fa67c69d1c28811d88d8f
doge <110700029+tototi932@users.noreply.github.com>
2022-09-14 21:09:43 -0400

    Update Home.tsx

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -434,7 +434,7 @@ const Home: React.FC = () => {
                 {t('Launchapd')}
               </Heading>
               <Text small fontSize="12px" color="text">
-                {t('Coming Soon.')}
+                {t('Web 3.0 Decentralize.')}
               </Text>
               <FeatBtn variant="secondary" scale="sm">
                 <ReactLink to="/Defi" color="text">
