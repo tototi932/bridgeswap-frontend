490527fe26b0e1aa613e056044ea33cb70fb1b0a
doge <110700029+tototi932@users.noreply.github.com>
2022-09-16 00:17:47 -0300

    Update index.tsx

--- src/views/Lottery/components/FAQ/index.tsx
+++ src/views/Lottery/components/FAQ/index.tsx
@@ -7,7 +7,7 @@ import styled from 'styled-components'
 
 const FaqContainer = styled(Flex)`
     border-radius: 20px;
-    background: linear-gradient(248.11deg, #0a0a0a 25.22%, #0a0a0a 122.1%);
+    background: linear-gradient(248.11deg, rgb(10, 10, 10) 25.22%, rgb(10, 10, 10) 122.1%);
     padding: 20px;
     justify-content: center;
     align-items: center;    
