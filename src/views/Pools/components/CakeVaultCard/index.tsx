328915028c32c3990fee0a83c8d4820b52b18ceb
doge <110700029+tototi932@users.noreply.github.com>
2022-09-15 21:32:55 -0300

    Update index.tsx

--- src/views/Pools/components/CakeVaultCard/index.tsx
+++ src/views/Pools/components/CakeVaultCard/index.tsx
@@ -46,8 +46,8 @@ const CakeVaultCard: React.FC<CakeVaultProps> = ({ pool, showStakedOnly }) => {
         <StyledCardHeader
           isStaking={accountHasSharesStaked}
           isAutoVault
-          earningTokenSymbol="BRIS"
-          stakingTokenSymbol="BRIS"
+          earningTokenSymbol="DGF"
+          stakingTokenSymbol="DGF"
         />
         <StyledCardBody isLoading={isLoading}>
           <AprRow pool={pool} performanceFee={performanceFeeAsDecimal} />
